9a6c5d8fa6f9a78394cc789de031e9da4013e5fc
Brandon Fuller <bjcfuller@uri.edu>
2017-09-20 12:49:48 -0400

    Replace all non-prefix instances of "_s" with "uri-modern" (should have done this when replacing prefixes a while back...)

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<!-- <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a> -->
+	<!-- <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'uri-modern' ); ?></a> -->
 
 	<header id="masthead" class="site-header" role="banner">
         
