1f0dbb7aa7454902bb20ea95a64f147e6515146d
Brandon Fuller <bjcfuller@uri.edu>
2017-11-09 12:40:10 -0500

    Remove bottom margin from page content on external landing pages (provides the ability to place content directly against the footer)

--- page-external-landing.php
+++ page-external-landing.php
@@ -11,7 +11,7 @@
 get_header(); 
 ?>
     
-    <main id="main" class="site-main" role="main">
+    <main id="main" class="site-main external-landing" role="main">
 
         <?php
         while ( have_posts() ) : the_post();
