e4d2684567f7ebf1445ec0388f1b6a57ff5aae37
Brandon Fuller <bjcfuller@uri.edu>
2017-11-16 12:20:24 -0500

    Use existing CSS hooks to style page titles instead of using an alternate h1 class

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <?php if ( get_field('pagetitle') === FALSE ) { ?>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title super">', '</h1>' ); ?>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
     <?php } ?>
 
