a7f4f220d8acd036bf8ce1c69fd356b215b78d31
Brandon Fuller <bjcfuller@uri.edu>
2018-01-08 11:02:05 -0500

    Add sitebar, breadcrumbs, and local nav to single posts template

--- single.php
+++ single.php
@@ -7,24 +7,34 @@
  * @package uri-modern
  */
 
-get_header(); ?>
+get_header();
+get_template_part( 'template-parts/sitebar' );
+?>
 
     <main id="main" class="site-main" role="main">
+        
+        <?php get_template_part( 'template-parts/breadcrumbs' ); ?>
 
-    <?php
-    while ( have_posts() ) : the_post();
+        <?php
+        if(has_nav_menu('menu-1')) {
+            get_template_part( 'template-parts/localnav' );
+        }
+        ?>
 
-        get_template_part( 'template-parts/content', get_post_format() );
+        <?php
+        while ( have_posts() ) : the_post();
 
-        the_post_navigation();
+            get_template_part( 'template-parts/content', get_post_format() );
 
-        // If comments are open or we have at least one comment, load up the comment template.
-        if ( comments_open() || get_comments_number() ) :
-            comments_template();
-        endif;
+            the_post_navigation();
 
-    endwhile; // End of the loop.
-    ?>
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif;
+
+        endwhile; // End of the loop.
+        ?>
 
     </main><!-- #main -->
 
