1c1dd39afe853319be4652557827d962dda36122
Brandon Fuller <bjcfuller@uri.edu>
2018-01-25 10:21:08 -0500

    Add favicon, pinned icon, and touch icons

--- header.php
+++ header.php
@@ -18,6 +18,12 @@
 
 <?php wp_head(); ?>
     
+<!-- Favicons -->
+<link rel="mask-icon" href="<?php echo get_template_directory_uri() . '/images/safari-pinned-tab.svg'; ?>" color="#0047c2">
+<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/images/favicon.png'; ?>">
+<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri() . '/images/apple-touch-icon.png'; ?>">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/images/apple-touch-icon-180x180.png'; ?>">
+
 <!-- Typekit embed code -->
 <script type="text/javascript" src="//use.typekit.com/qcq6uhe.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
