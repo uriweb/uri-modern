bdcef7a92bb3137fe321ed6f3085b2e91dc59172
John Pennypacker <jpennypacker@uri.edu>
2018-02-22 10:48:50 -0500

    added pleasant tabs to featured image template

--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -18,25 +18,25 @@
 	?>
 		<div class="featured-image">
 			<figure>
-			<?php if ( $link_to_fullsize ): ?>
-			<a href="<?php print $image[0] ?>" rel="lightbox[<?php print $post->ID; ?>]">
-			<?php endif; ?>
-			<?php if ( $link_to_article ): ?>
-			<a href="<?php print $url ?>">
-			<?php endif; ?>
-			<?php
-				$width = ( is_single() || is_page() ) ? 1200 : 300 ;
-				the_post_thumbnail( array( $width, NULL ) );
-			?>
-			<?php if ( $link_to_fullsize || $link_to_article): ?>
-			</a>
-			<?php endif; ?>
-			<?php
-			$figcaption = uri_modern_get_thumbnail_caption($post);
-			if ( ( is_single() || is_page() ) && !empty( $figcaption ) ):
-			?>
-			<figcaption class="wp-caption"><?php print $figcaption; ?></figcpation>
-			<?php endif; ?>
+				<?php if ( $link_to_fullsize ): ?>
+					<a href="<?php print $image[0] ?>" rel="lightbox[<?php print $post->ID; ?>]">
+				<?php endif; ?>
+				<?php if ( $link_to_article ): ?>
+					<a href="<?php print $url ?>">
+				<?php endif; ?>
+				<?php
+					$width = ( is_single() || is_page() ) ? 1200 : 300 ;
+					the_post_thumbnail( array( $width, NULL ) );
+				?>
+				<?php if ( $link_to_fullsize || $link_to_article): ?>
+					</a>
+				<?php endif; ?>
+				<?php
+					$figcaption = uri_modern_get_thumbnail_caption($post);
+					if ( ( is_single() || is_page() ) && !empty( $figcaption ) ):
+				?>
+				<figcaption class="wp-caption"><?php print $figcaption; ?></figcpation>
+				<?php endif; ?>
 			</figure>
 		</div>
 
