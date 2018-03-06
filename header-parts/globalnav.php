83d7f63dfd787f04b5fdcf3c8339f9d7613e1f2b
Brandon Fuller <bjcfuller@uri.edu>
2018-03-06 13:09:21 -0500

    Add mobile styles

--- header-parts/globalnav.php
+++ header-parts/globalnav.php
@@ -7,11 +7,12 @@
  */
 
 ?>
-
-    <ul class="content-width">
-        <li><a href="https://www.uri.edu/admissions">Admissions</a></li>
-        <li><a href="https://www.uri.edu/academics">Academics</a></li>
-        <li><a href="https://www.uri.edu/research">Research</a></li>
-        <li><a href="https://www.uri.edu/campus-life">Campus Life</a></li>
-        <li><a href="https://www.uri.edu/about">About</a></li>
+    <input type="checkbox" id="globalnav-toggle" role="presentation" aria-label="Open the global navigation menu when browsing on mobile">
+    <label for="globalnav-toggle" id="globalnav-label"><span>Main Menu</span></label>
+    <ul id="globalnav-menu" class="content-width" role="menu">
+        <li rule="menuitem"><a href="https://www.uri.edu/admissions">Admissions</a></li>
+        <li rule="menuitem"><a href="https://www.uri.edu/academics">Academics</a></li>
+        <li rule="menuitem"><a href="https://www.uri.edu/research">Research</a></li>
+        <li rule="menuitem"><a href="https://www.uri.edu/campus-life">Campus Life</a></li>
+        <li rule="menuitem"><a href="https://www.uri.edu/about">About</a></li>
     </ul>
