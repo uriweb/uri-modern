cece92f6d6e7705936e6e085675954cd10a4938a
Brandon Fuller <bjcfuller@uri.edu>
2018-03-16 13:55:14 -0400

    Use cl-menu component for the localnav if the component library is installed

--- header-parts/localnav.php
+++ header-parts/localnav.php
@@ -9,7 +9,14 @@
 ?>
 
 <div id="localnav">
+    <?php 
+    if (function_exists('uri_cl_shortcode_menu')) :
+        echo do_shortcode('[cl-menu depth=2 title="Site Menu" id="cl-localnav"]');
+    else : ?>
+    
     <input type="checkbox" id="lnmenu-toggle" role="presentation">
     <label for="lnmenu-toggle" id="lnmenu"><span>Site Menu</span></label>
     <?php wp_nav_menu( array('theme_location' => 'menu-1', 'container' => '', 'fallback_cb' => false) ); ?>
+    
+    <?php endif ?>
 </div>
