dfffc7e7bef20f138f7481ab3029061a483be0ce
Brandon Fuller <bjcfuller@uri.edu>
2018-03-29 13:39:28 -0400

    Style tags and categories differently; hide tags/cats screen reader text

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -47,12 +47,13 @@ if ( ! function_exists( 'uri_modern_entry_footer' ) ) :
 	function uri_modern_entry_footer() {
 		// Hide category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
-			$categories_list = get_the_category_list( );
+			/* translators: used between list items, there is a space after the comma */
+			$categories_list = get_the_category_list( esc_html__( ' ', 'uri' ) );
 			if ( $categories_list && uri_modern_categorized_blog() ) {
 				printf( '<span class="cat-links"><span class="screen-reader-text">Posted in:</span>' . esc_html__( '%1$s', 'uri' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 			}
 
-			/* translators: used between list items */
+			/* translators: used between list items, there is a space after the comma */
 			$tags_list = get_the_tag_list( '<span class="screen-reader-text">Tagged as:</span>', esc_html__( '', 'uri' ) );
 			if ( $tags_list ) {
 				printf( '<span class="tags-links">' . esc_html__( '%1$s', 'uri' ) . '</span>', $tags_list ); // WPCS: XSS OK.
