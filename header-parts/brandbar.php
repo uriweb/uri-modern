d4ee1681ad75ad334029a28b54ece90d4b2af63e
Brandon Fuller <bjcfuller@uri.edu>
2018-04-10 10:50:56 -0400

    Italicize <em> elements (fixes #63); add label to searchbox input (fixes #64); change global nav "Admission" link/name (fixes #66); increase visibility of "Work at URI" footer link (fixes #67)

--- header-parts/brandbar.php
+++ header-parts/brandbar.php
@@ -17,6 +17,7 @@
 			<form id="gs" method="get" action="https://www.uri.edu/search" name="global_general_search_form">
 				<input type="hidden" name="cx" value="016863979916529535900:17qai8akniu" />
 				<input type="hidden" name="cof" value="FORID:11" />
+                <label id="gs-query-label" for="gs-query">Searchbox</label>
 				<input role="searchbox" name="q" id="gs-query" value="<?php print ( isset( $_GET['q'] ) ) ? htmlentities( $_GET['q'] ) : ''; ?>" type="text" placeholder="Search" />
 				<input type="submit" id="gs-submit" class="searchsubmit" name="searchsubmit" value="Search" />
 			</form>
