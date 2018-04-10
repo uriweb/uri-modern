39ccab0c8cf5985b9dcaf5a8731ba50867954012
Brandon Fuller <bjcfuller@uri.edu>
2018-04-10 11:50:51 -0400

    Move duplicate header code to head.php

--- header-parts/head.php
+++ header-parts/head.php
@@ -2,11 +2,16 @@
 /**
  * Template part for displaying the document head.
  *
+ * This template is used by all header*.php templates and displays everything from <!DOCTYPE html> to <div id="page">
+ *
  * @package uri-modern
  */
 
 ?>
 
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,3 +27,16 @@
 
 
 </head>
+    
+<body <?php body_class(); ?>>
+	
+<?php
+	$gtm = uri_modern_gtm_value();
+if ( ! empty( $gtm ) ) {
+	echo '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=' . $gtm . '" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>';
+}
+?>
+	
+<div id="page" class="site">
+	<!-- <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'uri' ); ?></a> -->
+
