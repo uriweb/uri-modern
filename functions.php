b69f237e85972a0f5329fa21668257e621a8edfc
Brandon Fuller <bjcfuller@uri.edu>
2018-04-23 16:27:57 -0400

    Consolidate image sizes

--- functions.php
+++ functions.php
@@ -60,16 +60,12 @@ if ( ! function_exists( 'uri_modern_setup' ) ) :
 		add_theme_support( 'post-thumbnails' );
 
 		// add custom URI image sizes
-		add_image_size( 'people', 1000, 1000 );
-		add_image_size( 'people-preview', 360, 360 );
-		add_image_size( 'news', 1000 );
-		add_image_size( 'news-preview', 200, 200 );
-		add_image_size( 'bigphone-column', 728 );
-		add_image_size( 'tablet-column', 1436 );
-		add_image_size( 'full-column', 2000 );
-		add_image_size( 'hero', 2560 );
-		add_image_size( 'card-small', 700 );
-		add_image_size( 'card-large', 1000 );
+		add_image_size( 'third_column', 364 );
+        add_image_size( 'half_column', 500 );
+        add_image_size( 'full_column', 1000 );
+        add_image_size( 'hero', 1280 );
+        add_image_size( 'full_column@2x', 2000 );
+        add_image_size( 'hero@2x', 2560 );
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
@@ -123,16 +119,12 @@ add_action( 'after_setup_theme', 'uri_modern_setup' );
 function uri_modern_custom_sizes( $sizes ) {
 	return array_merge(
 		 $sizes, array(
-			 'people' => __( 'People' ),
-			 'people-preview' => __( 'People (preview)' ),
-			 'news' => __( 'News Item' ),
-			 'news-preview' => __( 'News Item (preview)' ),
-			 'bigphone-column' => __( 'Phone' ),
-			 'tablet-column' => __( 'Tablet' ),
-			 'full-column' => __( 'Desktop' ),
+			 'third_column' => __( 'Third Column' ),
+			 'half_column' => __( 'Half Column' ),
+			 'full_column' => __( 'Full Column' ),
 			 'hero' => __( 'Hero' ),
-			 'card-small' => __( 'Card (small)' ),
-			 'card-large' => __( 'Card (large)' ),
+             'full_column@2x' => __( 'Full Column @2x' ),
+			 'hero@2x' => __( 'Hero @2x' ),
 		 )
 		);
 }
