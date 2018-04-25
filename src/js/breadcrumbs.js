1dbfb210c59aae8d3d1c34f8db827409f55391d7
Brandon Fuller <bjcfuller@uri.edu>
2018-04-25 15:02:45 -0400

    Adjust breadcrumb styles; fix code styles

--- src/js/breadcrumbs.js
+++ src/js/breadcrumbs.js
@@ -6,46 +6,46 @@
 
 ( function() {
 
-    'use strict';
+	'use strict';
 
-    window.addEventListener( 'load', initBreadcrumbs, false );
+	window.addEventListener( 'load', initBreadcrumbs, false );
 
-    function initBreadcrumbs() {
+	function initBreadcrumbs() {
 
-        var BC = {}, div = {}, i;
+		var BC = {}, div = {}, i;
 
-        BC.el = document.getElementById( 'breadcrumbs' );
-        BC.ol = BC.el.querySelector( 'ol' );
-        BC.crumbs = BC.ol.querySelectorAll( 'li' );
+		BC.el = document.getElementById( 'breadcrumbs' );
+		BC.ol = BC.el.querySelector( 'ol' );
+		BC.crumbs = BC.ol.querySelectorAll( 'li' );
 
-        BC.el.classList.add( 'has-js' );
+		BC.el.classList.add( 'has-js' );
 
-        div.el = document.createElement( 'div' );
+		div.el = document.createElement( 'div' );
 
-        div.span = document.createElement( 'span' );
-        div.span.innerHTML = BC.crumbs[BC.crumbs.length - 2].querySelector( 'a' ).innerHTML;
-        div.el.appendChild( div.span );
+		div.span = document.createElement( 'span' );
+		div.span.innerHTML = BC.crumbs[BC.crumbs.length - 2].querySelector( 'a' ).innerHTML;
+		div.el.appendChild( div.span );
 
-        div.span = document.createElement( 'span' );
-        div.span.innerHTML = BC.crumbs[BC.crumbs.length - 1].innerHTML;
-        div.el.appendChild( div.span );
+		div.span = document.createElement( 'span' );
+		div.span.innerHTML = BC.crumbs[BC.crumbs.length - 1].innerHTML;
+		div.el.appendChild( div.span );
 
-        div.el.addEventListener( 'click', handleClick.bind( null, BC.el ) );
+		div.el.addEventListener( 'click', handleClick.bind( null, BC.el ) );
 
-        BC.el.insertBefore( div.el, BC.ol );
+		BC.el.insertBefore( div.el, BC.ol );
 
-    }
+	}
 
-    function handleClick( el ) {
+	function handleClick( el ) {
 
-        var c = 'active';
+		var c = 'active';
 
-        if ( el.classList.contains( c ) ) {
-            el.classList.remove( c );
-        } else {
-            el.classList.add( c );
-        }
+		if ( el.classList.contains( c ) ) {
+			el.classList.remove( c );
+		} else {
+			el.classList.add( c );
+		}
 
-    }
+	}
 
 })();
