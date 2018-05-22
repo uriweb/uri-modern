abc7b54024a53ba4c918081516a393499123dec5
John Pennypacker <jpennypacker@uri.edu>
2018-05-21 20:09:15 -0400

    adjust breadcrumbs so that they don't parse pagination; wrap "Archive: " in archive titles in screen reader span

--- functions.php
+++ functions.php
@@ -526,3 +526,43 @@ function uri_modern_add_slug_body_class( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', 'uri_modern_add_slug_body_class' );
+
+/**
+ * Add a span around the title prefix so that the prefix can be hidden with CSS
+ * if desired.
+ * Note that this will only work with LTR languages.
+ * @see https://www.binarymoon.co.uk/2017/02/hide-archive-title-prefix-wordpress/
+ *
+ * @param string $title Archive title.
+ * @return string Archive title with inserted span around prefix.
+ */
+function uri_modern_hide_archive_title( $title ) {
+	// Skip if the site isn't LTR, this is visual, not functional.
+	// Should try to work out an elegant solution that works for both directions.
+	if ( is_rtl() ) {
+		return $title;
+	}
+
+	// Split the title into parts so we can wrap them with spans.
+	$title_parts = explode( ': ', $title, 2 );
+
+	// Glue it back together again.
+	if ( ! empty( $title_parts[1] ) ) {
+		$title = wp_kses(
+			$title_parts[1],
+			array(
+				'span' => array(
+					'class' => array(),
+				),
+			)
+		);
+		$title = '<span class="screen-reader-text">' . esc_html( $title_parts[0] ) . ': </span>' . $title;
+	}
+
+	return $title;
+
+}
+
+add_filter( 'get_the_archive_title', 'uri_modern_hide_archive_title' );
+
+
