8887630da0b4b87939e91a760275a875e24ac50c
Brandon Fuller <bjcfuller@uri.edu>
2018-07-26 10:10:58 -0400

    Enqueue theme styles in the visual editor... needs some work/customization and/or a toggle switch

--- functions.php
+++ functions.php
@@ -366,6 +366,14 @@ add_action( 'wp_enqueue_scripts', 'uri_modern_scripts' );
 
 
 /**
+ * Enable styles in the WYSIWYG Editor
+ */
+if ( is_admin() ) {
+	add_editor_style( get_template_directory_uri() . '/style.css', __FILE__ );
+}
+
+
+/**
  * Gets the current WP path as known by Apache, not WordPress.
  *
  * @param bool $strip is a switch to strip slashes from the end of the URL.
