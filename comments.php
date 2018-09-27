f84905960a0f67af6e5e2336ad8008bb2a6bda33
Brandon Fuller <bjcfuller@uri.edu>
2018-09-27 09:43:22 -0400

    Adjust comment navigation styles and syntax

--- comments.php
+++ comments.php
@@ -40,8 +40,8 @@ if ( post_password_required() ) {
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'uri' ); ?></h2>
 			<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'uri' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'uri' ) ); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older', 'uri' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer', 'uri' ) ); ?></div>
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-above -->
@@ -63,8 +63,8 @@ if ( post_password_required() ) {
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'uri' ); ?></h2>
 			<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'uri' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'uri' ) ); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older', 'uri' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer', 'uri' ) ); ?></div>
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-below -->
