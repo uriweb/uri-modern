b52875b229dd289e2e9fad2b66ddd3b500f318b2
Brandon Fuller <bjcfuller@uri.edu>
2018-11-01 08:39:32 -0400

    Revert "Update link text color to comply with both WCAG A and AAA standards"
    This reverts commit 42626c55e283e6af1dc7d2e1e966086c6d036f69.

--- header-parts/head.php
+++ header-parts/head.php
@@ -20,7 +20,7 @@
 <?php wp_head(); ?>
 	
 <!-- Favicons -->
-<link rel="mask-icon" href="<?php echo get_template_directory_uri() . '/images/safari-pinned-tab.svg'; ?>" color="#044CD5">
+<link rel="mask-icon" href="<?php echo get_template_directory_uri() . '/images/safari-pinned-tab.svg'; ?>" color="#005eff">
 <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/images/favicon.png'; ?>">
 <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri() . '/images/apple-touch-icon.png'; ?>">
 <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/images/apple-touch-icon-180x180.png'; ?>">
