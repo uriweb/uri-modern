161c8a93fd83147965a56e2b30c22bad72a32abb
Brandon Fuller <bjcfuller@uri.edu>
2018-12-11 14:41:28 -0500

    Fix bug that caused single posts to not honor the "hide title" layout option

--- template-parts/content.php
+++ template-parts/content.php
@@ -19,7 +19,9 @@
 
 		the_title( '<h1 class="entry-title">', '</h1>' );
 
-	} else {
+	}
+
+	if ( ! is_single() ) {
 
 		the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 
