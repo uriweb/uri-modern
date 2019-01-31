8e959650d4257ab0e7a600f05a967107f85b8af1
John Pennypacker <jpennypacker@uri.edu>
2019-01-31 13:22:25 -0500

    removed policies

--- search-filter/results.php
+++ search-filter/results.php
@@ -14,15 +14,7 @@
 
 $search = ( ! empty( $_REQUEST['_sf_s'] ) ) ? $_REQUEST['_sf_s'] : false;
 
-
-$is_table = false;
-$is_policies = false;
-
 $site_name = strtolower( get_bloginfo( 'name' ) );
-if ( false !== strpos( $site_name, 'policies' ) ) { // @todo: find a better condition
-	$is_table = true;
-	$is_policies = true;
-}
 
 
 $query_id = $query->query['search_filter_id'];
@@ -47,19 +39,6 @@ if ( $query->have_posts() ) : ?>
 		</div>
 	
 		<div class="search-filter-results-posts ">
-			<?php if ( $is_table ) : ?>
-			<table class="search-filter-results">
-			<?php if ( $is_policies ) : ?>
-			<thead>
-			<tr>
-				<th>Policy</th>
-				<th>Category</th>
-				<th>Procedure</th>
-			</tr>
-			</thead>
-			<?php endif; ?>
-			<tbody>
-			<?php endif; ?>
 			
 			<?php
 			while ( $query->have_posts() ) {
@@ -67,18 +46,11 @@ if ( $query->have_posts() ) : ?>
 
 				if ( get_post_type() === 'people' ) { // it's a people post, customize result display
 					include 'people.php';
-				} elseif ( $is_policies ) { // it's a polcy
-					include 'policies.php';
 				} else { // not a people post, use default search and filter template
 					include 'default.php';
 				}
 			} // end while
 			?>
-			
-			<?php if ( $is_table ) : ?>
-				</tbody>
-				</table>
-			<?php endif; ?>
 			
 		</div>
 
@@ -99,47 +71,9 @@ if ( $query->have_posts() ) : ?>
 	
 	</div>
 
-			<?php if ( $is_table ) : ?>
-			
-			<script>
-
-(function($) {
-
-	$(document).ready( initExcerpter );
-
-	function initExcerpter() {
-
-		// hide all of the excerpt rows
-		$("tr.excerpt").toggleClass('excerpt-hidden');
-
-		$("table.search-filter-results thead tr").eq(0).append("<th></th>");
-		
-		// add an toggler widget to each row
-		$("td.policy").each(function(){
-			var excerpt = $(this).parents("tr").next();
-			if( $(excerpt).hasClass('excerpt')) {
-				$(this).parent("tr").append('<td><span class="excerpt-toggler">details</span></td>').click(function(){
-					$(excerpt).toggleClass('excerpt-hidden');
-				});
-				// since we added a td, increase the colspan of the excerpt row by one
-				var cols = $("td", excerpt).attr('colspan') * 1;
-				$("td", excerpt).attr('colspan', cols + 1);
-			}
-		});
-		
-	}
-
-})(jQuery);
-
-
-			</script>
-			
-			
-			<?php endif; ?>
-
 	
 <?php else : ?>
 
-	<p>No Results Found.</p>
+	<p class="no-results">No Results Found.</p>
 
 <?php endif; ?>
