e96ba3cd0c23cbb6628e7e0731d4e4638614af5c
Brandon Fuller <bjcfuller@uri.edu>
2019-04-10 10:19:59 -0400

    Restructure shame script to use helper functions; use mixins for styling shame messages

--- src/js/shame.js
+++ src/js/shame.js
@@ -4,6 +4,9 @@
  * @package uri-modern
  */
 
+// jshint esversion: 6
+// jscs:disable requireVarDeclFirst
+
 ( function() {
 
 	'use strict';
@@ -11,10 +14,91 @@
 	var data = {
 		'elClass': 'shamed-element',
 		'messageClass': 'shame-message',
-		'issues': 0
+		'issues': {
+			'total': 0,
+			'errors': 0,
+			'warnings': 0,
+			'suggestions': 0
+		}
 	};
 
 	document.addEventListener( 'DOMContentLoaded', init, false );
+
+	class Shame {
+
+		/**
+		 * Iterate over the tests
+		 * @param specs obj The element information
+		 * @param tests obj The tests to perform
+		 */
+		static iterateTests( specs, tests ) {
+
+			var i, j, test = {};
+
+			for ( i = 0; i < tests.length; i++ ) {
+
+				test.message = tests[i].message;
+				test.type = tests[i].type;
+
+				for ( j = 0; j < tests[i].selectors.length; j++ ) {
+					test.selector = tests[i].selectors[j];
+					Shame.runTest( specs.tag, specs.class, test );
+				}
+
+			}
+
+		}
+
+		/**
+		 * Do the test
+		 * @param tag str The tag to search for
+		 * @param classname str The wrapper class to pass to Shame.display()
+		 * @param test obj The test information
+		 */
+		static runTest( tag, classname, test ) {
+
+			var els, i;
+
+			els = data.main.querySelectorAll( tag + '[' + test.selector + ']' );
+
+			for ( i = 0; i < els.length; i++ ) {
+
+				Shame.display( els[i], classname, test.message, test.type );
+
+			}
+
+		}
+
+		/**
+		 * Display the shame
+		 * @param el el The element to shame
+		 * @param classname str The class to give the wrapper
+		 * @param message str The message to display
+		 * @param type str The issue type
+		 */
+		static display( el, classname, message, type ) {
+
+			var wrapper, div;
+
+			el.classList.add( data.elClass );
+
+			++data.issues.total;
+			++data.issues[type + 's'];
+
+			wrapper = document.createElement( 'span' );
+			wrapper.className = 'shame-wrapper ' + classname + ' shame-type-' + type;
+
+			div = document.createElement( 'div' );
+			div.className = data.messageClass;
+			div.innerHTML = message;
+			wrapper.appendChild( div );
+
+			el.parentNode.insertBefore( wrapper, el.nextSibling );
+			wrapper.appendChild( el );
+
+		}
+
+	}
 
 	function init() {
 
@@ -40,108 +124,90 @@
 
 	}
 
-	function displayStatus() {
-
-		var div, syntax;
-
-		syntax = ( 1 == data.issues ) ? 'issue' : 'issues';
-
-		div = document.createElement( 'div' );
-		div.className = 'shame-status';
-		div.innerHTML = 'This page has ' + data.issues + ' ' + syntax + '.';
-
-		data.main.insertBefore( div, data.main.firstChild );
-
-	}
-
-	function displayShame( el, classname, message ) {
-
-		var wrapper, div;
-
-		el.classList.add( data.elClass );
-
-		++data.issues;
-
-		wrapper = document.createElement( 'span' );
-		wrapper.className = 'shame-wrapper ' + classname;
-
-		div = document.createElement( 'div' );
-		div.className = data.messageClass;
-		div.innerHTML = message;
-		wrapper.appendChild( div );
-
-		el.parentNode.insertBefore( wrapper, el.nextSibling );
-		wrapper.appendChild( el );
-
-	}
-
 	function shameImages() {
 
-		var tests, img, i, j;
+		var specs, tests;
+
+		specs = {
+			tag: 'img',
+			class: 'shamed-image'
+		};
 
 		tests = [
 			{
-				selector: 'alt=""',
-				message: 'Alt attribute required'
+				selectors: ['alt=""'],
+				message: 'Alt attribute required',
+				type: 'error'
 			},
 			{
-				selector: 'src^="file:"',
-				message: 'No sourcing local resources'
+				selectors: ['src^="file:"'],
+				message: 'No sourcing local resources',
+				type: 'error'
 			}
 		];
 
-		for ( i = 0; i < tests.length; i++ ) {
-
-			img = data.main.querySelectorAll( 'img[' + tests[i].selector + ']' );
-
-			for ( j = 0; j < img.length; j++ ) {
-
-				displayShame( img[j], 'shamed-image', tests[i].message );
-
-			}
-
-		}
+		Shame.iterateTests( specs, tests );
 
 	}
 
 	function shameLinks() {
 
-		var tests, i, j, a;
+		var specs, tests;
+
+		specs = {
+			tag: 'a',
+			class: 'shamed-link'
+		};
 
 		tests = [
 			{
-				selector: 'href="#"',
-				message: '"#" is not a valid URL'
+				selectors: ['href="#"'],
+				message: '"#" is not a valid URL',
+				type: 'error'
 			},
 			{
-				selector: 'href^="javascript:"',
-				message: 'No JavaScript in links'
+				selectors: ['href^="javascript:"'],
+				message: 'No JavaScript in links',
+				type: 'error'
 			},
 			{
-				selector: 'href^="file:"',
-				message: 'No linking to local resources'
+				selectors: ['href^="file:"'],
+				message: 'No linking to local resources',
+				type: 'error'
 			},
 			{
-				selector: 'href^="///"',
-				message: 'Link href syntax error'
+				selectors: ['href^="///"'],
+				message: 'Link href syntax error',
+				type: 'error'
 			},
 			{
-				selector: 'href$=".pdf"',
-				message: 'PDF: Consider linking to a webpage instead'
+				selectors: ['href$=".pdf"'],
+				message: 'PDF: Consider linking to a webpage instead',
+				type: 'suggestion'
+			},
+			{
+				selectors: [
+					'href$=".doc"',
+					'href$=".docx"',
+					'href$=".docm"',
+					'href$=".xls"',
+					'href$=".xlm"',
+					'href$=".xlsx"',
+					'href$=".xlsm"',
+					'href$=".ppt"',
+					'href$=".pps"',
+					'href$=".pptx"',
+					'href$=".pptm"',
+					'href$=".ppsx"',
+					'href$=".sldx"',
+					'href$=".sldm"'
+					],
+				message: 'Not all users may be able to open this file.  Provide a download link to the required software.',
+				type: 'warning'
 			}
 		];
 
-		for ( i = 0; i < tests.length; i++ ) {
-
-			a = data.main.querySelectorAll( 'a[' + tests[i].selector + ']' );
-
-			for ( j = 0; j < a.length; j++ ) {
-
-				displayShame( a[j], 'shamed-link', tests[i].message );
-
-			}
-
-		}
+		Shame.iterateTests( specs, tests );
 
 	}
 
@@ -161,11 +227,33 @@
 			if ( ids[x] > 1 ) {
 				els = data.main.querySelectorAll( '[id="' + x + '"]' );
 				for ( i = 0; i < els.length; i++ ) {
-					displayShame( els[i], 'shamed-duplicate-id', 'No duplicate ids ("' + x + '")' );
+					Shame.display( els[i], 'shamed-duplicate-id', 'No duplicate ids ("' + x + '")', 'error' );
 				}
 			}
 
 		}
+
+	}
+
+	function displayStatus() {
+
+		var div, plural, string;
+
+		div = document.createElement( 'div' );
+		div.className = 'shame-status';
+
+		plural = ( 1 == data.issues.errors ) ? '' : 's';
+		string = 'This page has ' + data.issues.errors + ' error' + plural;
+
+		plural = ( 1 == data.issues.warnings ) ? '' : 's';
+		string += ', ' + data.issues.warnings + ' warning' + plural;
+
+		plural = ( 1 == data.issues.suggestions ) ? '' : 's';
+		string += ', and ' + data.issues.suggestions + ' suggestion' + plural + '.';
+
+		div.innerHTML = string;
+
+		data.main.insertBefore( div, data.main.firstChild );
 
 	}
 
