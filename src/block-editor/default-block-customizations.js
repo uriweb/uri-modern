5aa031f847d75938f2f0dd3799ce15281a81bf30
John Pennypacker <jpennypacker@uri.edu>
2019-12-17 09:44:59 -0500

    fixes gulpfile; toggles title in block editor

--- src/block-editor/default-block-customizations.js
+++ src/block-editor/default-block-customizations.js
@@ -12,11 +12,31 @@
 
 	wp.domReady(
 		() => {
-			// wp.blocks.unregisterBlockType( 'core/button' );
+			wp.blocks.unregisterBlockType( 'core/button' );
+			// Possible alternatives
 			// wp.blocks.unregisterBlockStyle( 'core/button', 'default' );
-			wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
-			wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
+			// wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
+			// wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
 		}
 	);
 
-})();
+	jQuery( document ).ready(
+		function($) {
+
+			// Featured image checkbox id: #acf-field_5afadb7ad2b38
+			// featured image checkbox id: #acf-field_502b9eb29fc45
+			// @see inc/layout-options.php
+
+			var f = $( "#acf-field_502b9eb29fc45" );
+			if( f.is(":checked") ) {
+				toggleTitle();
+			}
+			f.on( 'change', toggleTitle );
+		}
+	);
+
+	function toggleTitle() {
+		jQuery(".editor-post-title").toggle(400);
+	}
+}
+)();
