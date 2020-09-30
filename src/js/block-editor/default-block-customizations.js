f3aca7b4ed501bfdded33afbe6f9005c45ddf599
John Pennypacker <jpennypacker@uri.edu>
2020-09-30 14:37:07 -0400

    removes the renamed rounded image styles

--- src/js/block-editor/default-block-customizations.js
+++ src/js/block-editor/default-block-customizations.js
@@ -11,6 +11,7 @@
 	wp.domReady(
 		() => {
 			wp.blocks.unregisterBlockStyle( 'core/image', 'circle-mask' );
+			wp.blocks.unregisterBlockStyle( 'core/image', 'rounded' );
 			wp.blocks.unregisterBlockStyle( 'core/table', 'stripes' );
 			wp.blocks.unregisterBlockStyle( 'core/separator', 'wide' );
 
