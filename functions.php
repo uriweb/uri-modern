dc3e1c5c224bc3255c2b3362dcdcee94474b3141
alexandragauss <alexandra_gauss@uri.edu>
2024-08-16 12:49:02 -0400

    Add jQuery as a dependency

--- functions.php
+++ functions.php
@@ -389,7 +389,7 @@ function uri_modern_scripts() {
 
 	wp_enqueue_script( 'uri-modern-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), uri_modern_cache_buster(), true );
 
-	wp_enqueue_script( 'uri-modern-scripts', get_template_directory_uri() . '/js/script.min.js', array(), uri_modern_cache_buster(), true );
+	wp_enqueue_script( 'uri-modern-scripts', get_template_directory_uri() . '/js/script.min.js', array('jquery'), uri_modern_cache_buster(), true );
 	wp_localize_script( 'uri-modern-scripts', 'URIMODERN', $values );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
