68060d9dcf48666541b32e75ae892285140cf677
alexandragauss <alexandra_gauss@uri.edu>
2025-11-04 12:43:12 -0500

    Add nav tags to global menu

--- header-parts/brandbar.php
+++ header-parts/brandbar.php
@@ -29,7 +29,7 @@
 
 				<div id="gateways">
 					<input type="checkbox" id="gateways-toggle" role="presentation" aria-label="Open the audience gateways menu when browsing on mobile">
-					<label for="gateways-toggle" id="gateways-label"><span><?php echo ( defined( 'URI_EASTER_EGGS' ) && URI_EASTER_EGGS === true ) ? '&#128017' : 'Resources for'; ?></span></label>
+					<label for="gateways-toggle" id="gateways-label"><span><?php echo ( defined( 'URI_EASTER_EGGS' ) && URI_EASTER_EGGS === true ) ? '&#128017' : 'Info for'; ?></span></label>
 						<nav id="gateways-menu" aria-label="Resources for Navigation">
 						<ul id="gateways-ul">
 						<li><a href="https://<?php uri_modern_the_domain(); ?>/gateway/future-students">Future Students</a></li>
--- header-parts/globalnav.php
+++ header-parts/globalnav.php
@@ -9,11 +9,13 @@
 ?>
 	<input type="checkbox" id="globalnav-toggle" role="presentation" aria-label="Open the global navigation menu when browsing on mobile">
 	<label for="globalnav-toggle" id="globalnav-label">Main Menu <span role="presentation">open/close</span></label>
-	<ul id="globalnav-menu" class="content-width" role="menu">
-		<li role="menuitem"><a href="https://<?php uri_modern_the_domain(); ?>/admission">Admission</a></li>
-		<li role="menuitem"><a href="https://<?php uri_modern_the_domain(); ?>/academics">Academics</a></li>
-		<li role="menuitem"><a href="https://<?php uri_modern_the_domain(); ?>/research">Research</a></li>
-		<li role="menuitem"><a href="https://<?php uri_modern_the_domain(); ?>/campus-life">Campus Life</a></li>
-		<li role="menuitem"><a href="https://<?php uri_modern_the_domain(); ?>/about">About</a></li>
-		<li role="menuitem"><a href="https://<?php uri_modern_the_domain(); ?>/giving">Giving</a></li>
+	<nav id="globalnav-menu" class="content-width" aria-labeledby="globalnav-label">
+	<ul>
+		<li><a href="https://<?php uri_modern_the_domain(); ?>/admission">Admission</a></li>
+		<li><a href="https://<?php uri_modern_the_domain(); ?>/academics">Academics</a></li>
+		<li><a href="https://<?php uri_modern_the_domain(); ?>/research">Research</a></li>
+		<li><a href="https://<?php uri_modern_the_domain(); ?>/campus-life">Campus Life</a></li>
+		<li><a href="https://<?php uri_modern_the_domain(); ?>/about">About</a></li>
+		<li><a href="https://<?php uri_modern_the_domain(); ?>/giving">Giving</a></li>
 	</ul>
+</nav>
