e5a1f908aeadb5b572bb8ab0de5c59864e20897c
alexandragauss <alexandra_gauss@uri.edu>
2025-12-22 11:10:10 -0500

    Fix spacing errors in youmenu.js

--- src/js/youmenu.js
+++ src/js/youmenu.js
@@ -2,24 +2,23 @@
  * You menu control
  */
 
-(function () {
+( function() {
     'use strict';
 
-    window.addEventListener('load', initYouMenu, false);
+    window.addEventListener( 'load', initYouMenu, false );
 
-    function initYouMenu()
-    {
-        const gateways = document.getElementById('gateways');
-        const input = document.getElementById('gateways-toggle');
+    function initYouMenu() {
+        const gateways = document.getElementById( 'gateways' );
+        const input = document.getElementById( 'gateways-toggle' );
 
         window.addEventListener(
             'click',
-            function ( e ) {
-                if (input.checked && ! gateways.contains(e.target) ) {
+            function( e ) {
+                if ( input.checked && ! gateways.contains( e.target ) ) {
                     input.checked = false;
                 }
             },
             false
         );
     }
-}());
+}() );
