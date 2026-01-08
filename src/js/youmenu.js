4885bcec02d3e474e4595908247e069c862c8493
alexandragauss <alexandra_gauss@uri.edu>
2026-01-08 12:43:45 -0500

    Add styles and functionality to restructured 'You' menu

--- src/js/youmenu.js
+++ src/js/youmenu.js
@@ -2,23 +2,41 @@
  * You menu control
  */
 
-( function() {
+(function () {
     'use strict';
 
-    window.addEventListener( 'load', initYouMenu, false );
+    window.addEventListener('load', initYouMenu, false);
 
     function initYouMenu() {
-        const gateways = document.getElementById( 'gateways' );
-        const input = document.getElementById( 'gateways-toggle' );
+        const gateways = document.getElementById('gateways-dropdown');
+        const input = document.getElementById('gateways-toggle');
 
-        window.addEventListener(
+        input.addEventListener(
             'click',
-            function( e ) {
-                if ( input.checked && ! gateways.contains( e.target ) ) {
-                    input.checked = false;
+            () => {
+                gateways.classList.toggle("show-dropdown");
+                input.classList.toggle("gateways-toggle-checked");
+
+                if (gateways.classList.contains("show-dropdown")) {
+                    input.setAttribute('aria-expanded', "true");
+                } else {
+                    input.setAttribute('aria-expanded', "false");
                 }
+
             },
             false
         );
+
+        window.addEventListener(
+            'click',
+            (e) => {
+                if (gateways.classList.contains("show-dropdown") && !gateways.contains(e.target) && !input.contains(e.target)) {
+                    gateways.classList.remove("show-dropdown");
+                    input.classList.remove("gateways-toggle-checked");
+                    input.setAttribute('aria-expanded', "false");
+                }
+            },
+            false
+        )
     }
-}() );
+}());
