9bc11ab8b9619d4ceabeae549dfada159f621fb6
alexandragauss <alexandra_gauss@uri.edu>
2026-01-20 15:29:32 -0500

    Fix spacing

--- src/js/youmenu.js
+++ src/js/youmenu.js
@@ -2,25 +2,25 @@
  * You menu control
  */
 
-(function () {
+( function() {
     'use strict';
 
-    window.addEventListener('load', initYouMenu, false);
+    window.addEventListener( 'load', initYouMenu, false );
 
     function initYouMenu() {
-        const gateways = document.getElementById('gateways-dropdown');
-        const input = document.getElementById('gateways-toggle');
+        const gateways = document.getElementById( 'gateways-dropdown' );
+        const input = document.getElementById( 'gateways-toggle' );
 
         input.addEventListener(
             'click',
             () => {
-                gateways.classList.toggle("show-dropdown");
-                input.classList.toggle("gateways-toggle-checked");
+                gateways.classList.toggle( 'show-dropdown' );
+                input.classList.toggle( 'gateways-toggle-checked' );
 
-                if (gateways.classList.contains("show-dropdown")) {
-                    input.setAttribute('aria-expanded', "true");
+                if ( gateways.classList.contains( 'show-dropdown' )) {
+                    input.setAttribute( 'aria-expanded', "true" );
                 } else {
-                    input.setAttribute('aria-expanded', "false");
+                    input.setAttribute( 'aria-expanded', "false" );
                 }
 
             },
@@ -29,14 +29,14 @@
 
         window.addEventListener(
             'click',
-            (e) => {
-                if (gateways.classList.contains("show-dropdown") && !gateways.contains(e.target) && !input.contains(e.target)) {
-                    gateways.classList.remove("show-dropdown");
-                    input.classList.remove("gateways-toggle-checked");
-                    input.setAttribute('aria-expanded', "false");
+            ( e ) => {
+                if ( gateways.classList.contains( 'show-dropdown' ) && ! gateways.contains( e.target ) && ! input.contains( e.target ) ) {
+                    gateways.classList.remove( 'show-dropdown' );
+                    input.classList.remove( 'gateways-toggle-checked' );
+                    input.setAttribute( 'aria-expanded', "false" );
                 }
             },
             false
-        )
+        );
     }
-}());
+}() );
