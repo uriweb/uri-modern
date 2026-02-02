bcffcac1397ddb7e48270082686cbc80cf68bca4
alexandragauss <alexandra_gauss@uri.edu>
2026-02-02 15:28:07 -0500

    Adjust styles for main menu button and dropwdown

--- header-parts/globalnav.php
+++ header-parts/globalnav.php
@@ -4,12 +4,9 @@
  * Use it by adding the shortcode [uri-modern-gn] to a customizer widget
  *
  * @package uri-modern
- * @todo use button for mobile interaction instead of checkbox
  */
 
 ?>
-	<!--<input type="checkbox" id="globalnav-toggle" role="presentation" aria-label="Open the global navigation menu when browsing on mobile">
-	<label for="globalnav-toggle" id="globalnav-label">Main Menu <span role="presentation">open/close</span></label>-->
 	<button id="globalnav-label" class="globalnav-button" aria-expanded="false" aria-label="Open the global navigation menu">Main Menu <span role="presentation">open/close</span></button>
 	<nav class="content-width globalnav-dropdown" id="globalnav-menu" aria-label="Global Site menu">
 	<ul>
